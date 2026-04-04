c1637258bf51c8cb24ea963f2463ac6efaff0489
vaibhavrajcs28-commits <vaibhav.raj.cs28@iilm.edu>
2026-04-04 10:41:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (93.69%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if n<=1:
+            return 1
+        a, b = 1, 1
+        for i in range(2, n+1):
+            a, b = b, a + b
+        
+        return b    
+        

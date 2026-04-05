bf6894fa5fa4315b3dc492732042c9f1f82378e5
vaibhavrajcs28-commits <vaibhav.raj.cs28@iilm.edu>
2026-04-05 15:40:21 +0530

    Time: 853 ms (6.61%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT e1.salary
+    FROM Employee e1
+    WHERE (
+        SELECT COUNT(DISTINCT e2.salary)
+        FROM Employee e2
+        WHERE e2.salary > e1.salary
+    ) = N - 1
+
+  );
+END
